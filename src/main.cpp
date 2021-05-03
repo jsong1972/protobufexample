66346850b08daa0b20ca4fdcd399941636b6c2cb
Jie Song <jsong1972@yahoo.com>
2021-05-03 00:17:15 -0500

    finish the project

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "proto/message.pb.h"
+
+int main(int argc, char const *argv[]) {
+    message::Message m;
+    std::cout << "Hello proto!" << std::endl;
+    return 0;
+}
